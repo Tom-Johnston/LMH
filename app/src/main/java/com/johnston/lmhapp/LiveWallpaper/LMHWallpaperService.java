5cef94e326b52fd314caa3e243abb16b32b6fa77
Tom-Johnston <tom.johnston5861@gmail.com>
2015-02-01 10:43:47 +0000

    Revert "Probably more failing at a live wallpaper."
    This reverts commit 03c125de4155145b04528f1cb49b741115873c8a.

--- app/src/main/java/com/johnston/lmhapp/LiveWallpaper/LMHWallpaperService.java
+++ app/src/main/java/com/johnston/lmhapp/LiveWallpaper/LMHWallpaperService.java
@@ -5,8 +5,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Handler;
 import android.os.Message;
 import android.view.SurfaceHolder;
@@ -21,7 +19,7 @@ import java.util.Random;
  */
 public class LMHWallpaperService extends android.service.wallpaper.WallpaperService {
 
-    //    This isn't even close to finished but I have fixed a few bugs in development and will commit this code.
+//    This isn't even close to finished but I have fixed a few bugs in development and will commit this code.
     @Override
     public Engine onCreateEngine() {
         return new LMHWallpaperEngine();
@@ -31,16 +29,13 @@ public class LMHWallpaperService extends android.service.wallpaper.WallpaperServ
 
     public class LMHWallpaperEngine extends Engine{
         ArrayList<Triangle> triangles;
-        ArrayList<Integer> onScreenTriangles;
-        ArrayList<Integer> targetColours;
-        ArrayList<Integer> currentColours;
+        ArrayList<Integer> onScreenTraingles;
+        ArrayList<Integer> colours;
         int[] color = new int[9];
         Handler drawHandler = new Handler();
         int numberOfTriangleGenerators;
-        Paint bottomLeftFillPaint1 = new Paint();
-        Paint bottomLeftStrokePaint = new Paint();
-        Random rand = new Random();
-
+        Canvas workingCanvas;
+        Bitmap workingBitmap;
 
         int width =1000;
         int height =2000;
@@ -50,7 +45,7 @@ public class LMHWallpaperService extends android.service.wallpaper.WallpaperServ
         Runnable drawRunner = new Runnable(){
             @Override
             public void run(){
-                initialDraw();
+                draw();
             }
         };
 
@@ -64,92 +59,85 @@ public class LMHWallpaperService extends android.service.wallpaper.WallpaperServ
             color[6] = (Color.parseColor("#FFD54F"));
             color[7] = (Color.parseColor("#E0E0E0"));
             color[8] = (Color.parseColor("#90A4AE"));
-            bottomLeftFillPaint1.setStyle(Paint.Style.FILL);
-            bottomLeftFillPaint1.setAntiAlias(true);
-            bottomLeftStrokePaint.setColor(Color.parseColor("#003D81"));
-            bottomLeftStrokePaint.setStyle(Paint.Style.STROKE);
-            bottomLeftStrokePaint.setAntiAlias(true);
-            bottomLeftStrokePaint.setStrokeWidth(1);
             System.out.println("Engine Started");
-
+        }
+        public void generateColours(){
+            colours = new ArrayList<>();
+            Random rand = new Random();
+            for(int i=0;i<triangles.size();i++){
+                colours.add(i,rand.nextInt(9));
+            }
+            checkOnScreen();
+            initialDraw();
         }
 
         public void generateTriangles() {
             drawHandler.removeCallbacks(drawRunner);
             numberOfTriangleGenerators++;
             System.out.println("Generating Triangles");
-            int numberOfIterations = 5;
+            int numberOfIterations = 3;
             final Handler handler = new Handler() {
                 @Override
                 public void handleMessage(Message message) {
-                    if(message.what==0){
-                        numberOfTriangleGenerators--;
-                        System.out.println(numberOfTriangleGenerators);
-                    }
+                    numberOfTriangleGenerators--;
                     if(numberOfTriangleGenerators==0){
-                        if(message.what==0){
-                            triangles = (ArrayList<Triangle>) message.obj;
-                        }else{
-                            targetColours = (ArrayList<Integer>) message.obj;
-                            System.out.println("Got target colours");
-                            generateRandomColours();
-                            checkOnScreen();
-                            initialDraw();
-                        }
+                        triangles = (ArrayList<Triangle>) message.obj;
+                        generateColours();
                     }
 
                 }
             };
-            new TriangleGeneratorAsync().execute(width,height,numberOfIterations,handler,getApplicationContext());
-        }
-
-        public void generateRandomColours() {
-            Random random = new Random();
-            currentColours = new ArrayList<>();
-            for (int i = 0; i < triangles.size(); i++) {
-                currentColours.add(color[random.nextInt(9)]);
-            }
-            System.out.println("Generated Random Colours");
+            new TriangleGeneratorAsync().execute(width,height,numberOfIterations,handler);
         }
 
         public void checkOnScreen(){
             Triangle triangle;
-            onScreenTriangles = new ArrayList<>();
+            onScreenTraingles = new ArrayList<>();
             for (int i = 0; i < triangles.size(); i++) {
                 triangle = triangles.get(i);
                 if(triangle.x1<width && triangle.y1<height||triangle.x2<width && triangle.y2<height||triangle.x3<width && triangle.y3<height) {
                     //We must render this triangle
-                    onScreenTriangles.add(i);
+                    onScreenTraingles.add(i);
                 }
             }
-            System.out.println("Checked for on screen triangles");
         }
 
-
-        public Rect computeDirtyRegion(Triangle triangle){
-            int minX = (int) Math.min(Math.min(triangle.x1,triangle.x2),triangle.x3);
-            int maxX = (int) Math.max(Math.max(triangle.x1,triangle.x2),triangle.x3)+1;
-            int minY = (int) Math.min(Math.min(triangle.y1,triangle.y2),triangle.y3);
-            int maxY = (int) Math.max(Math.max(triangle.y1,triangle.y2),triangle.y3)+1;
-            return new Rect(minX,minY,maxX,maxY);
-
-        }
 
         public void initialDraw(){
             long startTime = System.currentTimeMillis();
+            int[] color = new int[9];
+            color[0] = (Color.parseColor("#BA68C8"));
+            color[1] = (Color.parseColor("#7986CB"));
+            color[2] = (Color.parseColor("#64B5F6"));
+            color[3] = (Color.parseColor("#4DB6AC"));
+            color[4] = (Color.parseColor("#81C784"));
+            color[5] = (Color.parseColor("#DCE775"));
+            color[6] = (Color.parseColor("#FFD54F"));
+            color[7] = (Color.parseColor("#E0E0E0"));
+            color[8] = (Color.parseColor("#90A4AE"));
 
+
+            Paint bottomLeftFillPaint1 = new Paint();
+            bottomLeftFillPaint1.setStyle(Paint.Style.FILL);
+            bottomLeftFillPaint1.setAntiAlias(true);
+            Paint bottomLeftStrokePaint = new Paint();
             bottomLeftStrokePaint.setColor(Color.parseColor("#000000"));
+            bottomLeftStrokePaint.setStyle(Paint.Style.STROKE);
+            bottomLeftStrokePaint.setAntiAlias(true);
+            bottomLeftStrokePaint.setStrokeWidth(2);
 
             Triangle triangle;
             int i;
+            Random rand = new Random();
+            Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+            workingBitmap = Bitmap.createBitmap(width, height, conf);
+            workingCanvas = new Canvas(workingBitmap);
 
-            int changedTriangle = onScreenTriangles.get(rand.nextInt(onScreenTriangles.size()));
-            currentColours.set(changedTriangle,targetColours.get(changedTriangle));
-
+            colours.set(onScreenTraingles.get(rand.nextInt(onScreenTraingles.size())),rand.nextInt(9));
             final  SurfaceHolder holder = getSurfaceHolder();
             Canvas canvas =  holder.lockCanvas();
-            for (i = 0; i < onScreenTriangles.size(); i++) {
-                triangle = triangles.get(onScreenTriangles.get(i));
+            for (i = 0; i < onScreenTraingles.size(); i++) {
+                triangle = triangles.get(onScreenTraingles.get(i));
                 Path path = new Path();
                 path.setFillType(Path.FillType.EVEN_ODD);
                 path.moveTo(triangle.x1, triangle.y1);
@@ -157,63 +145,53 @@ public class LMHWallpaperService extends android.service.wallpaper.WallpaperServ
                 path.lineTo(triangle.x3, triangle.y3);
                 path.close();
 
-                    bottomLeftFillPaint1.setColor(currentColours.get(onScreenTriangles.get(i)));
-                    canvas.drawPath(path, bottomLeftFillPaint1);
-                    canvas.drawPath(path, bottomLeftStrokePaint);
+                bottomLeftFillPaint1.setColor(color[colours.get(onScreenTraingles.get(i))]);
+                workingCanvas.drawPath(path, bottomLeftFillPaint1);
+                workingCanvas.drawPath(path, bottomLeftStrokePaint);
             }
-            holder.unlockCanvasAndPost(canvas);
+            holder.unlockCanvasAndPost(workingCanvas);
             drawHandler.postDelayed(drawRunner,100);
-            System.out.println("Initial Draw");
             System.out.println(System.currentTimeMillis()-startTime);
             return;
         }
 
-//        public void draw(){
-//            if(workingCanvas==null){
-//                initialDraw();
-//            }else{
-//                showFrameTime++;
-//                startTime = System.currentTimeMillis();
-//                int changedTriangle = onScreenTriangles.get(rand.nextInt(onScreenTriangles.size()));
-//                currentColours.set(changedTriangle,targetColours.get(changedTriangle));
-//
-//                triangle = triangles.get(changedTriangle);
-//                path.setFillType(Path.FillType.EVEN_ODD);
-//                path.moveTo(triangle.x1, triangle.y1);
-//                path.lineTo(triangle.x2, triangle.y2);
-//                path.lineTo(triangle.x3, triangle.y3);
-//                path.close();
-//                final  SurfaceHolder holder = getSurfaceHolder();
-//                Canvas canvas = holder.lockCanvas();
-//                canvas.drawBitmap(workingBitmap,0,0,null);
-//                canvas.setBitmap(workingBitmap);
-//                System.out.println("Start");
-//                System.out.println(System.currentTimeMillis()-startTime);
-//
-//                if(currentColours.get(changedTriangle)==-1){
-//                    bottomLeftFillPaint1.setColor(Color.WHITE);
-//                    bottomLeftStrokePaint.setColor(Color.parseColor("#003D81"));
-//                }else if(targetColours.get(changedTriangle)==Color.parseColor("#ffffff")){
-//                    bottomLeftFillPaint1.setColor(Color.parseColor("#ffffff"));
-//                    bottomLeftStrokePaint.setColor(Color.parseColor("#ffffff"));
-//                }else{
-//                    bottomLeftFillPaint1.setColor(Color.parseColor("#002147"));
-//                    bottomLeftStrokePaint.setColor(Color.parseColor("#003D81"));
-//                }
-//                System.out.println(System.currentTimeMillis()-startTime);
-//
-//                canvas.drawPath(path, bottomLeftFillPaint1);
-//                canvas.drawPath(path, bottomLeftStrokePaint);
-//                System.out.println(System.currentTimeMillis() - startTime);
-//                holder.unlockCanvasAndPost(canvas);
-//                drawHandler.removeCallbacks(drawRunner);
-//                drawHandler.postDelayed(drawRunner,50);
-//
-//
-//                System.out.println(System.currentTimeMillis()-startTime);
-//
-//            }
-//        }
+        public void draw(){
+            if(workingCanvas==null){
+                initialDraw();
+            }else{
+                final  SurfaceHolder holder = getSurfaceHolder();
+                Canvas canvas = holder.lockCanvas();
+                long startTime = System.currentTimeMillis();
+                Random rand = new Random();
+                int changedTriangle = onScreenTraingles.get(rand.nextInt(onScreenTraingles.size()));
+                colours.set(changedTriangle,rand.nextInt(9));
+                Triangle triangle = triangles.get(changedTriangle);
+                Path path = new Path();
+                path.setFillType(Path.FillType.EVEN_ODD);
+                path.moveTo(triangle.x1, triangle.y1);
+                path.lineTo(triangle.x2, triangle.y2);
+                path.lineTo(triangle.x3, triangle.y3);
+                path.close();
+
+
+                Paint bottomLeftFillPaint1 = new Paint();
+                bottomLeftFillPaint1.setStyle(Paint.Style.FILL);
+                bottomLeftFillPaint1.setAntiAlias(true);
+                Paint bottomLeftStrokePaint = new Paint();
+                bottomLeftStrokePaint.setColor(Color.parseColor("#000000"));
+                bottomLeftStrokePaint.setStyle(Paint.Style.STROKE);
+                bottomLeftStrokePaint.setAntiAlias(true);
+                bottomLeftStrokePaint.setStrokeWidth(2);
+
+                bottomLeftFillPaint1.setColor(color[colours.get(changedTriangle)]);
+                workingCanvas.drawPath(path, bottomLeftFillPaint1);
+                workingCanvas.drawPath(path, bottomLeftStrokePaint);
+                drawHandler.removeCallbacks(drawRunner);
+                drawHandler.postDelayed(drawRunner,100);
+                System.out.println(System.currentTimeMillis()-startTime);
+                holder.unlockCanvasAndPost(workingCanvas);
+            }
+        }
 
 
         @Override
@@ -225,6 +203,7 @@ public class LMHWallpaperService extends android.service.wallpaper.WallpaperServ
                     generateTriangles();
                 }else{
                     System.out.println("Engine Initialised with triangles");
+                    initialDraw();
                 }
             } else {
                 drawHandler.removeCallbacks(drawRunner);
--- app/src/main/java/com/johnston/lmhapp/LiveWallpaper/TriangleGeneratorAsync.java
+++ app/src/main/java/com/johnston/lmhapp/LiveWallpaper/TriangleGeneratorAsync.java
@@ -2,35 +2,36 @@ package com.johnston.lmhapp.LiveWallpaper;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.AsyncTask;
 import android.os.Handler;
 
-import com.johnston.lmhapp.ImageGeneratorAsync;
-import com.johnston.lmhapp.R;
 import com.johnston.lmhapp.Triangle;
 
-import java.lang.reflect.Array;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
- * Created by Tom on 21/01/2015.
+ * Created by Tom on 05/11/2014.
  */
-public class TriangleGeneratorAsync extends AsyncTask<Object,Void,Void> {
+public class TriangleGeneratorAsync extends AsyncTask<Object, Void, Void> {
+
     @Override
     protected Void doInBackground(Object... params) {
         System.out.println("Generate Triangles");
-        int desiredWidth = (int) params[0];
-        int desiredHeight = (int)params[1];
         int sizex = (Integer) params[0];
         int sizey = (Integer) params[1];
         int numberOfIterationsToDo = (int) params[2];
         Handler handler = (Handler) params[3];
-        Context context = (Context) params[4];
 
         if (sizex > 2 * sizey) {
 //            Scale up y to match sizex;
@@ -39,9 +40,6 @@ public class TriangleGeneratorAsync extends AsyncTask<Object,Void,Void> {
 //            Scale up x to match y
             sizex = 2 * sizey;
         }
-
-
-
 
 //        Starting bottom triangle
         ArrayList<Triangle> triangles = new ArrayList<>();
@@ -66,65 +64,17 @@ public class TriangleGeneratorAsync extends AsyncTask<Object,Void,Void> {
 
 
         for (int j = 0; j < numberOfIterationsToDo; j++) {
-            triangles = NextIteration(triangles,desiredWidth,desiredHeight);
+            triangles = NextIteration(triangles);
         }
         handler.obtainMessage(0,triangles).sendToTarget();
-        handler.obtainMessage(1,generateColours(triangles,context,desiredHeight,desiredWidth)).sendToTarget();
         System.out.println("Finished Generating Triangles");
         return null;
+
     }
 
-
-
-    public ArrayList<Integer> generateColours(ArrayList<Triangle> triangles,Context context, int desiredWidth, int desiredHeight){
-        ArrayList<Integer> targetColours = new ArrayList<>();
-        int oxfordBlue = Color.parseColor("#002147");
-        int white = Color.parseColor("#ffffff");
-        int alpha = -1;
-
-
-
-        int margin = desiredWidth/8;
-        Bitmap logo = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_for_background);
-        float scale = ((float)logo.getHeight())/((float)desiredWidth-2*margin); //We will scale the height so the height of the logo fits the height of the display with margins.
-
-        Triangle triangle;
-        int i;
-        int x;
-        int y;
-
-        for (i = 0; i < triangles.size(); i++) {
-            triangle = triangles.get(i);
-            Path path = new Path();
-            path.setFillType(Path.FillType.EVEN_ODD);
-            path.moveTo(triangle.x1, triangle.y1);
-            path.lineTo(triangle.x2, triangle.y2);
-            path.lineTo(triangle.x3, triangle.y3);
-            path.close();
-            int periodInX = (int) (((float)logo.getWidth())/scale + margin); // Repeat the logo horizontally.
-            int xMargin = (desiredWidth-periodInX*((int)desiredWidth/periodInX)+margin)/2;       // Center the logos.
-            x = (int) (scale*(triangle.middlex%periodInX-xMargin));
-            y = (int) (scale*(triangle.middley-margin));
-            if(0<=x&&x<logo.getWidth()&&0<=y&&y<logo.getHeight()) {
-                if(logo.getPixel(x,y)==oxfordBlue){
-                    targetColours.add(oxfordBlue);
-                }else if(Color.alpha(logo.getPixel(x,y))==0){
-                    targetColours.add(alpha);
-                }else{
-//                    This contains the white triangles and the triangles that are in between the specified colours due to anti-aliasing.
-                    targetColours.add(white);
-                }
-            }else{
-                targetColours.add(alpha);
-            }
-        }
-        return targetColours;
-    }
-
-
-
-    public ArrayList<Triangle> NextIteration(ArrayList<Triangle> passedTriangles,int desiredWidth, int desiredHeight) {
+    public ArrayList<Triangle> NextIteration(ArrayList<Triangle> passedTriangles) {
         ArrayList<Triangle> triangles = new ArrayList<>();
+        float oneOverSquareRoot5 = 0.4472135955f;
         Triangle newTriangle;
         int numberOfTriangles = passedTriangles.size();
         Triangle triangle;
@@ -132,15 +82,11 @@ public class TriangleGeneratorAsync extends AsyncTask<Object,Void,Void> {
 
             triangle = passedTriangles.get(i);
 
-            if(triangle.x1>desiredWidth && triangle.x2>desiredWidth && triangle.x3>desiredWidth  ||  triangle.y1>desiredHeight && triangle.y2>desiredHeight && triangle.y3>desiredHeight){
-                continue;
-            }
-
 
 //            Triangle 1
             newTriangle = new Triangle();
-            newTriangle.x1 = triangle.x3 - 0.2f * (triangle.x3 - triangle.x2);
-            newTriangle.y1 = triangle.y3 - 0.2f * (triangle.y3 - triangle.y2);
+            newTriangle.x1 = triangle.x3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.x3 - triangle.x2);
+            newTriangle.y1 = triangle.y3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.y3 - triangle.y2);
 
             newTriangle.x2 = triangle.x1;
             newTriangle.y2 = triangle.y1;
@@ -148,49 +94,40 @@ public class TriangleGeneratorAsync extends AsyncTask<Object,Void,Void> {
             newTriangle.x3 = triangle.x3;
             newTriangle.y3 = triangle.y3;
 
-            newTriangle.middlex = (2f/3f) * newTriangle.x1 +  (1f/3f) * newTriangle.x2;
-            newTriangle.middley = (2f/3f) * newTriangle.y1 +  (1f/3f) * newTriangle.y3;
-
             triangles.add(newTriangle);
 
 //            Triangle 2
             newTriangle = new Triangle();
-            newTriangle.x1 = (float) (0.5 * triangle.x1 + 0.5 * (triangle.x3 - 0.2f * (triangle.x3 - triangle.x2)));
-            newTriangle.y1 = (float) (0.5 * triangle.y1 + 0.5 * (triangle.y3 - 0.2f * (triangle.y3 - triangle.y2)));
+            newTriangle.x1 = (float) (0.5 * triangle.x1 + 0.5 * (triangle.x3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.x3 - triangle.x2)));
+            newTriangle.y1 = (float) (0.5 * triangle.y1 + 0.5 * (triangle.y3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.y3 - triangle.y2)));
 
             newTriangle.x2 = (float) (0.5 * (triangle.x1 + triangle.x2));
             newTriangle.y2 = (float) (0.5 * (triangle.y1 + triangle.y2));
 
-            newTriangle.x3 = triangle.x3 - 0.2f * (triangle.x3 - triangle.x2);
-            newTriangle.y3 = triangle.y3 - 0.2f * (triangle.y3 - triangle.y2);
-
-            newTriangle.middlex = (2f/3f) * newTriangle.x1 +  (1f/3f) * newTriangle.x2;
-            newTriangle.middley = (2f/3f) * newTriangle.y1 +  (1f/3f) * newTriangle.y3;
+            newTriangle.x3 = triangle.x3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.x3 - triangle.x2);
+            newTriangle.y3 = triangle.y3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.y3 - triangle.y2);
 
             triangles.add(newTriangle);
 
 //            Triangle 3
 
             newTriangle = new Triangle();
-            newTriangle.x1 = triangle.x3 - 0.2f * 3 * (triangle.x3 - triangle.x2);
-            newTriangle.y1 = triangle.y3 - 0.2f * 3 * (triangle.y3 - triangle.y2);
+            newTriangle.x1 = triangle.x3 - oneOverSquareRoot5 * oneOverSquareRoot5 * 3 * (triangle.x3 - triangle.x2);
+            newTriangle.y1 = triangle.y3 - oneOverSquareRoot5 * oneOverSquareRoot5 * 3 * (triangle.y3 - triangle.y2);
 
-            newTriangle.x2 = triangle.x3 - 0.2f * (triangle.x3 - triangle.x2);
-            newTriangle.y2 = triangle.y3 - 0.2f * (triangle.y3 - triangle.y2);
+            newTriangle.x2 = triangle.x3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.x3 - triangle.x2);
+            newTriangle.y2 = triangle.y3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.y3 - triangle.y2);
 
             newTriangle.x3 = (float) (0.5 * (triangle.x1 + triangle.x2));
             newTriangle.y3 = (float) (0.5 * (triangle.y1 + triangle.y2));
-
-            newTriangle.middlex = (2f/3f) * newTriangle.x1 +  (1f/3f) * newTriangle.x2;
-            newTriangle.middley = (2f/3f) * newTriangle.y1 +  (1f/3f) * newTriangle.y3;
 
             triangles.add(newTriangle);
 
             //            Triangle 4
 
             newTriangle = new Triangle();
-            newTriangle.x1 = (float) (0.5 * triangle.x1 + 0.5 * (triangle.x3 - 0.2f * (triangle.x3 - triangle.x2)));
-            newTriangle.y1 = (float) (0.5 * triangle.y1 + 0.5 * (triangle.y3 - 0.2f * (triangle.y3 - triangle.y2)));
+            newTriangle.x1 = (float) (0.5 * triangle.x1 + 0.5 * (triangle.x3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.x3 - triangle.x2)));
+            newTriangle.y1 = (float) (0.5 * triangle.y1 + 0.5 * (triangle.y3 - oneOverSquareRoot5 * oneOverSquareRoot5 * (triangle.y3 - triangle.y2)));
 
             newTriangle.x2 = (float) (0.5 * (triangle.x1 + triangle.x2));
             newTriangle.y2 = (float) (0.5 * (triangle.y1 + triangle.y2));
@@ -198,25 +135,19 @@ public class TriangleGeneratorAsync extends AsyncTask<Object,Void,Void> {
             newTriangle.x3 = triangle.x1;
             newTriangle.y3 = triangle.y1;
 
-            newTriangle.middlex = (2f/3f) * newTriangle.x1 +  (1f/3f) * newTriangle.x2;
-            newTriangle.middley = (2f/3f) * newTriangle.y1 +  (1f/3f) * newTriangle.y3;
-
             triangles.add(newTriangle);
 
             //            Triangle 5
 
             newTriangle = new Triangle();
-            newTriangle.x1 = triangle.x3 - 0.2f * 3 * (triangle.x3 - triangle.x2);
-            newTriangle.y1 = triangle.y3 - 0.2f * 3 * (triangle.y3 - triangle.y2);
+            newTriangle.x1 = triangle.x3 - oneOverSquareRoot5 * oneOverSquareRoot5 * 3 * (triangle.x3 - triangle.x2);
+            newTriangle.y1 = triangle.y3 - oneOverSquareRoot5 * oneOverSquareRoot5 * 3 * (triangle.y3 - triangle.y2);
 
             newTriangle.x2 = triangle.x2;
             newTriangle.y2 = triangle.y2;
 
             newTriangle.x3 = (float) (0.5 * (triangle.x1 + triangle.x2));
             newTriangle.y3 = (float) (0.5 * (triangle.y1 + triangle.y2));
-
-            newTriangle.middlex = (2f/3f) * newTriangle.x1 +  (1f/3f) * newTriangle.x2;
-            newTriangle.middley = (2f/3f) * newTriangle.y1 +  (1f/3f) * newTriangle.y3;
 
             triangles.add(newTriangle);
 
@@ -225,5 +156,4 @@ public class TriangleGeneratorAsync extends AsyncTask<Object,Void,Void> {
 
         return triangles;
     }
-
 }
